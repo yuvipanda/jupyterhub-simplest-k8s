8a37c242ae2364ec4c546ca446e4edf1fa99f0cd
YuviPanda <yuvipanda@gmail.com>
2016-08-31 18:53:48 -0700

    Make container + config namespace aware
    Use the downward API to allow the hub container to discover
    which namespace it is running in. This is then used as
    the default namespace to spawn new instances in.

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -6,15 +6,25 @@ c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.JupyterHub.proxy_cmd = '/usr/local/bin/nchp'
 c.JupyterHub.ip = '0.0.0.0'
 
-c.KubeSpawner.kube_namespace = 'default'
-c.KubeSpawner.kube_api_endpoint = 'https://kubernetes'
+c.KubeSpawner.kube_namespace = os.environ.get('POD_NAMESPACE', 'default')
+c.KubeSpawner.kube_api_endpoint = 'https://{host}:{port}'.format(
+    host=os.environ['KUBERNETES_SERVICE_HOST'],
+    port=os.environ['KUBERNETES_SERVICE_PORT']
+)
+# Disable SSL Auth for now. It's not the end of the world because we
+# are inside the cluster, but we should fix this by moving to kubesession
+# soon! FIXME
+c.KubeSpawner.kube_ca_path = False
 c.KubeSpawner.start_timeout = 60 * 5  # Upto 5 minutes, first pulls can be really slow
 
 # Our simplest user image! Optimized to just... start, and be small!
 c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
 
 # The spawned containers need to be able to talk to the hub!
-c.KubeSpawner.hub_ip_connect = '%s:%s' % (os.environ['HUB_SERVICE_HOST'], os.environ['HUB_SERVICE_PORT'])
+c.KubeSpawner.hub_ip_connect = '{host}:{port}'.format(
+    host=os.environ['HUB_SERVICE_HOST'],
+    port=os.environ['HUB_SERVICE_PORT']
+)
 
 # Do not use any authentication at all
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
